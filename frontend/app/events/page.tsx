a7dba79f53a095f739bd5e1f115663cfa4abfd52
bobliuuu <mmmzzz66g@gmail.com>
2024-11-20 18:38:15 -0500

    Fix small thing

--- frontend/app/events/page.tsx
+++ frontend/app/events/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Calendar from "@/components/calendar";
-import Particles from "@/components/particles"
 
 export default async function Events() {
   return (
     <div className="rounded-md border border-gray-200 w-11/12 h-10/12 py-4 shadow">
-      <Particles />
+      <Calendar />
     </div>
   );
 }
